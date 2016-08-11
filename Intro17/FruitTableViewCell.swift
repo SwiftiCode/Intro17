49bfb4001f2ae795b860a42ae7170ac6ba53fd5e
SwiftiCode <swifticode@203.203-211-151.static.qala.com.sg>
2016-08-11 14:03:13 +0800

    Complete

---
+++ Intro17/FruitTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  FruitTableViewCell.swift
+//  Intro17
+//
+//  Created by SwiftiCode on 11/8/16.
+//  Copyright © 2016 SwiftiCode. All rights reserved.
+//
+
+import UIKit
+
+class FruitTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    @IBOutlet weak var cellFruitImageView: UIImageView!
+    @IBOutlet weak var cellFruitLabel: UILabel!
+
+    
+    // MARK: Default Template
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
